576edcbd21c5c6ef87034b5e17c995124fb6a22e
Brandon Valosek <bvalosek@gmail.com>
2021-03-21 22:47:51 -0500

    remove BVAL721 constructor arg

--- packages/solidity-contracts/contracts/BVAL721.sol
+++ packages/solidity-contracts/contracts/BVAL721.sol
@@ -5,7 +5,7 @@ import "./CoreERC721.sol";
 
 // @bvalosek ERC-721 Token
 contract BVAL721 is CoreERC721 {
-  constructor(string memory baseURI) CoreERC721(CollectionOptions({
+  constructor() CoreERC721(CollectionOptions({
     name: "@bvalosek",
     symbol: "BVAL-NFT",
     feeBps: 1000,
