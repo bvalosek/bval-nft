9c5409035764bec2b92aaf8c2384b7bad656b8e9
Brandon Valosek <bvalosek@gmail.com>
2021-03-28 17:58:20 -0500

    creating metadata nodes with remote file references

--- packages/site-token-gallery/plugins/gatsby-plugin-source-tokens/gatsby-node.js
+++ packages/site-token-gallery/plugins/gatsby-plugin-source-tokens/gatsby-node.js
@@ -1,26 +1,83 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-// const { createRemoteFileNode } = require('gatsby-source-filesystem');
+const { createRemoteFileNode } = require('gatsby-source-filesystem');
 
 const allTokenData = require('@bvalosek/token-manifest/data/tokens.json');
 const allSequenceData = require('@bvalosek/token-manifest/data/sequences.json');
 const allCollectionData = require('@bvalosek/token-manifest/data/collections.json');
 
 /** given a uri with the IPFS protocol, return a http URL */
-// const ipfsGatewayUrl = (ipfsUri) => ipfsUri.replace('ipfs://', 'https://gateway.pinata.cloud');
+const ipfsGatewayUrl = (ipfsUri) => ipfsUri.replace('ipfs://', 'https://gateway.pinata.cloud/');
 
-/** gatsby plugin to generate nodes from the token data */
-exports.sourceNodes = async ({ actions, createNodeId, createContentDigest }) => {
+/**
+ * gatsby hook to modifiy existing inferred graph types
+ */
+exports.createSchemaCustomization = ({ actions, schema }) => {
+  // we only need to define fields that extend the inferred set of fields that
+  // are created in the sourceNodes hook below
+  const TokenType = schema.buildObjectType({
+    name: 'Token',
+    fields: {
+      metadata: {
+        type: '[Metadata]',
+        // in sourceNodes, we set metadata to an array of metadata node IDs.
+        // this will "hydrate" those IDs in the entire node
+        resolve(source, args, context, info) {
+          return context.nodeModel.getNodesByIds({ ids: source.metadata, type: 'Metadata' });
+        },
+      },
+    },
+    interfaces: ['Node'],
+  });
+
+  actions.createTypes([TokenType]);
+};
+
+/**
+ * gatsby hook used to create nodes from external data sources (in this case,
+ * the data files from the token manifest)
+ */
+exports.sourceNodes = async ({ actions, createNodeId, createContentDigest, getCache }) => {
   const sequenceNodeId = (number) => createNodeId(`sequence ${number}`);
   const collectionNodeId = (version) => createNodeId(`collection ${version}`);
 
-  // create all tokens
   for (const token of allTokenData) {
+    // create all metadata nodes for this token
+    const metadataNodeIds = [];
+    for (const metadata of token.metadata) {
+      const id = createNodeId(`metadata ${metadata.cid}`);
+      metadataNodeIds.push(id);
+
+      // create a "remote file node" referencing the IPFS image so we can
+      // leverage all the image processing gatsy stuff for each metadata node
+      const imageNode = await createRemoteFileNode({
+        url: ipfsGatewayUrl(metadata.content.image),
+        getCache,
+        createNode: actions.createNode,
+        createNodeId,
+        parentNodeId: id,
+      });
+
+      actions.createNode({
+        id,
+        cid: metadata.cid,
+        content: metadata.content,
+        remoteImage___NODE: imageNode.id,
+        internal: {
+          type: 'Metadata',
+          contentDigest: createContentDigest(JSON.stringify(metadata)),
+        },
+      });
+    }
+
+    // create the actual token node
     actions.createNode({
       id: createNodeId(token.tokenId),
-      ...token,
+      tokenId: token.tokenId,
+      tokenPayload: token.source.token,
+      name: token.source.name,
+      slug: token.slug,
+      metadata: metadataNodeIds,
       sequence___NODE: sequenceNodeId(token.source.token.sequenceNumber),
-      parent: null,
-      children: [],
       internal: {
         type: 'Token',
         contentDigest: createContentDigest(JSON.stringify(token)),
