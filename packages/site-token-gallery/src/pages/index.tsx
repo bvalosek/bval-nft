833b1562498a28ee65e4dcf9c52222532be20106
Brandon Valosek <bvalosek@gmail.com>
2021-04-01 20:16:17 -0500

    group by sequence on home page

--- packages/site-token-gallery/src/pages/index.tsx
+++ packages/site-token-gallery/src/pages/index.tsx
@@ -9,17 +9,24 @@ import { useTokens } from '../hooks/tokens';
 
 export const HomePage: FunctionComponent = () => {
   const tokens = useTokens();
+  const sequences = new Set(tokens.map((t) => t.sequence.sequenceNumber));
+  const grouped = [...sequences].map((s) => tokens.filter((t) => t.sequence.sequenceNumber === s)).reverse();
+
   return (
     <Application>
       <HeadTags />
       <PageWithFooter>
         <HomeHero />
-        <PageSection>
-          <h1>Recently Minted</h1>
-        </PageSection>
-        <PageSection>
-          <TokenGrid tokens={tokens} />
-        </PageSection>
+        {grouped.map((tokens) => (
+          <>
+            <PageSection>
+              <h1>{tokens[0].sequence.source.name}</h1>
+            </PageSection>
+            <PageSection>
+              <TokenGrid tokens={tokens} />
+            </PageSection>
+          </>
+        ))}
       </PageWithFooter>
     </Application>
   );
