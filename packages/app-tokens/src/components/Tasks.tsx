d86bd2b877c0c71bc1d4c5011d5b7f20ee484c14
Brandon Valosek <bvalosek@gmail.com>
2021-04-01 20:37:14 -0500

    lol gross

--- packages/app-tokens/src/components/Tasks.tsx
+++ packages/app-tokens/src/components/Tasks.tsx
@@ -10,11 +10,13 @@ import { useWeb3Strict } from '../util/web3';
 export const Tasks: FunctionComponent = () => {
   const { library } = useWeb3Strict();
 
-  const tokens = allTokens.filter((t) => t.source.token.sequenceNumber === 1);
-  const sequence = allSequences.find((s) => s.sequenceNumber === 1);
+  // TODO: something better than this!
+  const tokens = allTokens.filter((t) => t.source.token.sequenceNumber === 2);
+  const sequence = allSequences.find((s) => s.sequenceNumber === 2);
 
   // TODO: allow storing of contract address in local storage by chainID
   const contractAddress = '0x02D91986F0C2B02830bDfC022f0dA83529B78334';
+  // const contractAddress = '0x826DDe34365ec31edFbEaCcC732fA6c0813eF7DA'; // rink
 
   const sdata: SequenceCreateData = {
     sequenceNumber: sequence.sequenceNumber,
@@ -37,7 +39,7 @@ export const Tasks: FunctionComponent = () => {
         {/* TODO: dynamically determine "tasks" to perform based on on-chain
         contract state and all data files */}
         <Button type="success" onClick={() => atomicMint(contractAddress, sdata, tdata, library.getSigner())}>
-          atomic mint ARCH-DESCENT
+          atomic mint {sequence.source.name}
         </Button>
       </div>
     </Container>
