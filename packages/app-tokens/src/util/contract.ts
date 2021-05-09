977025780ec8086e7ad484a4a7a411e0875c10a5
Brandon Valosek <bvalosek@gmail.com>
2021-05-09 16:19:47 -0500

    car checkp

--- packages/app-tokens/src/util/contract.ts
+++ packages/app-tokens/src/util/contract.ts
@@ -1,11 +1,17 @@
 import { Contract, ContractFactory, Signer } from 'ethers';
 import { TransactionResponse } from '@ethersproject/abstract-provider';
 
-import { BVAL721 } from '@bvalosek/solidity-contracts';
+import { BVAL721, BVAL_WELLSPRING } from '@bvalosek/solidity-contracts';
 import { SequenceCreateData, TokenMintData } from '../../../token-manifest/node_modules/@bvalosek/lib-tokens';
 
 export const deployBVAL721 = async (signer: Signer): Promise<Contract> => {
   const factory = new ContractFactory(BVAL721.abi, BVAL721.bytecode, signer);
+  const contract = await factory.deploy();
+  return contract;
+};
+
+export const deployWellspring = async (signer: Signer): Promise<Contract> => {
+  const factory = new ContractFactory(BVAL_WELLSPRING.abi, BVAL_WELLSPRING.bytecode, signer);
   const contract = await factory.deploy();
   return contract;
 };
