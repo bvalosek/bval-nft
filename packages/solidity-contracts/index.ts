f544172ae9d404e8505bb4fb33adafda7a6717c1
Brandon Valosek <bvalosek@gmail.com>
2021-05-09 09:24:28 -0500

    wellspring

--- packages/solidity-contracts/index.ts
+++ packages/solidity-contracts/index.ts
@@ -1,5 +1,7 @@
 import bval721 from './build/contracts/BVAL721.json';
 import bval20 from './build/contracts/BVAL20.json';
+import bvalWellspring from './build/contracts/BVALWellspring.json';
 
 export const BVAL721 = bval721;
 export const BVAL20 = bval20;
+export const BVAL_WELLSPRING = bvalWellspring;
