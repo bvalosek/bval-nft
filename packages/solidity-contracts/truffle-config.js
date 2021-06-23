977bb07a492393cebf4264254209ac736e436e97
Brandon Valosek <bvalosek@gmail.com>
2021-06-23 07:18:22 -0500

    truffle verify working with the gon

--- packages/solidity-contracts/truffle-config.js
+++ packages/solidity-contracts/truffle-config.js
@@ -29,7 +29,11 @@ module.exports = {
       skipDryRun: true,
     },
     polygon: {
+      provider: () => new HDWalletProvider({ provider: 'https://rpc-mainnet.matic.network' }),
       network_id: 137,
+      networkCheckTimeout: 1000000,
+      timeoutBlocks: 200,
+      skipDryRun: true,
     },
   },
   compilers: {
