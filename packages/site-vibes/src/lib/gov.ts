0ed2d1867e50f6d64f4500bf246ceba2d64e5e66
Brandon Valosek <bvalosek@gmail.com>
2021-06-27 21:28:48 -0500

    import checked in abi

--- packages/site-vibes/src/lib/gov.ts
+++ packages/site-vibes/src/lib/gov.ts
@@ -1,6 +1,6 @@
 import { JsonRpcProvider } from '@ethersproject/providers';
 import { BigNumber, Contract } from 'ethers';
-import { VOTE_POWER } from '@bvalosek/solidity-contracts';
+import VOTE_POWER from '@bvalosek/solidity-contracts/deployed-contracts/VotePower-2021-06-27.json';
 import { getContracts } from './contracts';
 
 export const getVotePower = async (provider: JsonRpcProvider, address: string): Promise<BigNumber> => {
