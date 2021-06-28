ff3bbcc81d8d6e43b7c6ba2ad98c145fa169f34d
Brandon Valosek <bvalosek@gmail.com>
2021-06-27 19:53:56 -0500

    spaces yikes

--- packages/solidity-contracts/contracts/mocks/MockPair.sol
+++ packages/solidity-contracts/contracts/mocks/MockPair.sol
@@ -8,32 +8,32 @@ import "../VibesStrategies.sol";
 // LP balance = 100
 // reserve0 = 500, reserve1 = 700
 contract MockPair is IPair {
-	address private _token0;
-	address private _token1;
+  address private _token0;
+  address private _token1;
 
-	constructor(address t0, address t1) {
-		_token0 = t0;
-		_token1 = t1;
-	}
+  constructor(address t0, address t1) {
+    _token0 = t0;
+    _token1 = t1;
+  }
 
-	function totalSupply() override external pure returns (uint) {
-		return 1000 * 10 ** 18;
-	}
+  function totalSupply() override external pure returns (uint) {
+    return 1000 * 10 ** 18;
+  }
 
-	function balanceOf(address) override external pure returns (uint) {
-		return 100 * 10 ** 18;
-	}
+  function balanceOf(address) override external pure returns (uint) {
+    return 100 * 10 ** 18;
+  }
 
-	function getReserves() override external pure returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast) {
-		return (500 * 10 ** 18, 700 * 10 ** 18, 15);
-	}
+  function getReserves() override external pure returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast) {
+    return (500 * 10 ** 18, 700 * 10 ** 18, 15);
+  }
 
-	function token0() override external view returns (address) {
-		return _token0;
-	}
+  function token0() override external view returns (address) {
+    return _token0;
+  }
 
-	function token1() override external view returns (address) {
-		return _token1;
-	}
+  function token1() override external view returns (address) {
+    return _token1;
+  }
 
 }
