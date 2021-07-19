6d2c186732dfed43c77676e51fbf5093ca7551ee
Brandon Valosek <bvalosek@gmail.com>
2021-07-18 22:09:36 -0500

    shell

--- packages/solidity-contracts/contracts/TestShell.sol
+++ packages/solidity-contracts/contracts/TestShell.sol
@@ -9,6 +9,13 @@ import '@openzeppelin/contracts/utils/Strings.sol';
 contract TestShell is MetadataResolver {
   using Strings for uint256;
 
+  string[] private _images = [
+    "ipfs://ipfs/QmaQhk8JTVTxNeWjA3aym7qTEDDE8B1mFssE1ufSN49Y8Y",
+    "ipfs://ipfs/QmQkAKH84WgjACadJ3Z4feNCEEozfWrt4Z32WXYmZfFkRn",
+    "ipfs://ipfs/QmVw3oFRMU5VTXsDaztaLL5hdZKajUCKKiH9cx4FyMEpZx",
+    "ipfs://ipfs/QmZ17NGQu6FiCdEAPcg7nUZYiA1gMkT7U9oZkECaQKCYo9"
+  ];
+
   function _computeName(MetaNFT nft, uint256 tokenId) override internal view returns (string memory) {
     return string(abi.encodePacked(nft.name(), " #", tokenId.toString()));
   }
@@ -16,8 +23,8 @@ contract TestShell is MetadataResolver {
   function _computeDescription(MetaNFT nft, uint256 tokenId) override internal view returns (string memory) {
     TokenViewData memory token = nft.getTokenData(tokenId);
     return string(abi.encodePacked(
-      "This is not a collectible; this is a self-sovereign modular application platform.\\n\\n",
-      "Your [NTEST#", tokenId.toString(), "] is your personal carrier wave for the PTEST protocol.\\n\\n",
+      "This is not a collectible; this is a tokenized, self-sovereign modular application platform.\\n\\n",
+      "A [NTEST] is your personal carrier wave for the PTEST protocol.\\n\\n",
       "Have you minted yours?\\n\\n",
       "---\\n\\n",
       "Originally minted by ", uint256(uint160(token.creator)).toHexString(20),
@@ -33,7 +40,9 @@ contract TestShell is MetadataResolver {
     return "https://website.com";
   }
 
-  function _computeImageUri(MetaNFT, uint256) override internal pure returns (string memory) {
-    return "ipfs://ipfs/QmWcFCdDeDsSbty62PSm6dE8Yr3CtRBXFg3YwsdGyHauLK";
+  function _computeImageUri(MetaNFT nft, uint256 tokenId) override internal view returns (string memory) {
+    TokenViewData memory token = nft.getTokenData(tokenId);
+    uint256 idx = token.seed % _images.length;
+    return _images[idx];
   }
 }
