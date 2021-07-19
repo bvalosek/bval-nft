57ee81b962c5dcc56e625e5b6a8f68678bd35d52
Brandon Valosek <bvalosek@gmail.com>
2021-07-18 20:29:04 -0500

    test shell

--- packages/solidity-contracts/contracts/TestShell.sol
+++ packages/solidity-contracts/contracts/TestShell.sol
@@ -7,25 +7,28 @@ import "./MetadataResolver.sol";
 import '@openzeppelin/contracts/utils/Strings.sol';
 
 contract TestShell is MetadataResolver {
-	using Strings for uint256;
+  using Strings for uint256;
 
-	function _computeName(MetaNFT nft, uint256 tokenId) override internal view returns (string memory) {
-		return string(abi.encodePacked(nft.name(), " #", tokenId.toString()));
-	}
+  function _computeName(MetaNFT nft, uint256 tokenId) override internal view returns (string memory) {
+    return string(abi.encodePacked(nft.name(), " #", tokenId.toString()));
+  }
 
-	function _computeDescription(MetaNFT nft, uint256 tokenId) override internal view returns (string memory) {
-		TokenViewData memory token = nft.getTokenData(tokenId);
-		return string(abi.encodePacked(
-			"This token was minted in block ", token.createdAtBlock.toString(),
-			" at timestamp ", token.createdAtTimestamp.toString(),
-			" by address ", uint256(uint160(token.creator)).toHexString(20), ".\\n\\n",
-			"isVip: ", token.isVip ? "true" : "false", ".\\n\\n",
-			"isCredit: ", token.isCredit ? "true" : "false", ".\\n\\n",
-			"seed: ", token.seed.toHexString(32)
-		));
-	}
+  function _computeDescription(MetaNFT nft, uint256 tokenId) override internal view returns (string memory) {
+    TokenViewData memory token = nft.getTokenData(tokenId);
+    return string(abi.encodePacked(
+      "This is not a collectible; this is a self-sovereign modular application platform.\\n\\n",
+      "Your [NTEST#", tokenId.toString(), "] is your personal carrier wave for the PTEST protocol.\\n\\n",
+      "https://website.com\\n\\n",
+      "---\\n\\n",
+      "Originally minted by ", uint256(uint160(token.creator)).toHexString(20),
+      " at timestamp ", token.createdAtTimestamp.toString(), "." ,
+      token.isVip
+        ? "\n\nThis NFT was the first [NTEST] minted by a founding member of PTEST."
+        : ""
+    ));
+  }
 
-	function _computeImageUri(MetaNFT, uint256) override internal pure returns (string memory) {
-		return "https://gateway.pinata.cloud/ipfs/QmWcFCdDeDsSbty62PSm6dE8Yr3CtRBXFg3YwsdGyHauLK";
-	}
+  function _computeImageUri(MetaNFT, uint256) override internal pure returns (string memory) {
+    return "https://gateway.pinata.cloud/ipfs/QmWcFCdDeDsSbty62PSm6dE8Yr3CtRBXFg3YwsdGyHauLK";
+  }
 }
