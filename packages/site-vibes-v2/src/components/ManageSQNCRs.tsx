9f25a590d94d7e6994b44caa87019e20984e3ed7
Brandon Valosek <bvalosek@gmail.com>
2021-07-25 20:26:10 -0500

    manage

--- packages/site-vibes-v2/src/components/ManageSQNCRs.tsx
+++ packages/site-vibes-v2/src/components/ManageSQNCRs.tsx
@@ -3,14 +3,61 @@ import { PageSection } from './PageSection';
 import { Connect } from './Connect';
 import { Content } from './Content';
 import { Title } from './Title';
+import { useWallet } from '../hooks/wallet';
+import { getVariantName } from '../web3/sqncr';
+import { SQNCR } from './SQNCR';
+import { ButtonGroup } from './ButtonGroup';
+import { Button } from './Button';
+import { makeStyles } from '@material-ui/styles';
+import { ThemeConfig } from '../Theme';
+
+const useStyles = makeStyles<ThemeConfig>((theme) => {
+  return {
+    header: {
+      fontWeight: 'bold',
+      color: theme.palette.accent.secondary,
+    },
+  };
+});
 
 export const ManageSQNCRs: FunctionComponent = () => {
+  const { accountView } = useWallet();
+  const classes = useStyles();
   return (
     <>
       <PageSection>
         <Content>
           <Connect>
             <Title>Manage SQNCRs</Title>
+            <table>
+              <thead>
+                <tr className={classes.header}>
+                  <th style={{ textAlign: 'left' }}>SQNCR</th>
+                  <th style={{ textAlign: 'left' }}>variant</th>
+                  <th style={{ textAlign: 'left' }}>shell</th>
+                  <th style={{ textAlign: 'left' }}>modules</th>
+                </tr>
+              </thead>
+              <tbody>
+                {accountView?.sqncrs.map((sqncr) => (
+                  <tr key={sqncr.tokenId}>
+                    <td>
+                      <SQNCR sqncr={sqncr} />
+                    </td>
+                    <td>{getVariantName(sqncr.variant)}</td>
+                    <td>DEFAULT.EXE</td>
+                    <td>(none)</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+            <ButtonGroup>
+              {accountView?.maxMints > accountView?.mintedSQNCRs ? (
+                <Button navTo="/sqncr/mint">🚀 MINT new SQNCR</Button>
+              ) : (
+                <Button disabled>max SQNCRs minted</Button>
+              )}
+            </ButtonGroup>
           </Connect>
         </Content>
       </PageSection>
